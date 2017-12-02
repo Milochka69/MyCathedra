adf4a4a53c32e4407d59bb4707cd5d1f66131e5e
Unknown <gaga-ya@hotmail.com>
2017-12-02 13:46:40 +0200

    added DBmanager and Model empty project

---
+++ MyCathedra.DatabaseManager.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCathedra.DatabaseManager.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ MyCathedra.DatabaseManager/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace MyCathedra.DatabaseManager
+{
+    public class Manager : DbContext
+    {
+        static Manager()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (Manager db = new Manager())
+                db.Database.Initialize(false);
+        }
+    }
+
+    class DbInitializer : DropCreateDatabaseAlways<Manager>
+    {
+        protected override void Seed(Manager context)
+        {
+            base.Seed(context);
+        }
+    }
+}
