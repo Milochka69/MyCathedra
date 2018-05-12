1f1722913290063a48e1a6fd37e577066890cac9
Петр Радилов <illusionist.nemo@gmail.com>
2018-05-12 12:57:35 +0300

    добавлено удаление

--- MyCathedra/MainWindow.xaml.cs
+++ MyCathedra/MainWindow.xaml.cs
@@ -48,7 +48,8 @@ namespace MyCathedra
                     {
                         Text = directoryChildren.Name,
                         PathSource = ItemPathSource(directoryChildren.Name),
-                        Style = FindResource("TileMenuItem") as Style
+                        Style = FindResource("TileMenuItem") as Style,
+                        FontSize = 15
                     };
 
                     item.Click += TileMenuItem_Click;
@@ -59,7 +60,9 @@ namespace MyCathedra
                 {
                     Content = stackPanel,
                     Header = directory,
-                    Margin = new Thickness(0.8)
+                    Margin = new Thickness(0.8),
+                    FontSize = 17,
+                    FontWeight = FontWeights.DemiBold
                 };
                 element.Expanded += Expander_Expanded;
                 _expanders.Add(element);
@@ -71,6 +74,8 @@ namespace MyCathedra
         {
             switch (name)
             {
+                case "":
+                    return Geometry.Parse("");
                 default:
                     return Geometry.Parse(
                         "M16.5,6.5A2,2,0,0,1,18.5,8.5A2,2,0,0,1,16.5,10.5A2,2,0,0,1,14.5,8.5A2,2,0,0,1,16.5,6.5 M16.5,12A3.5,3.5,0,0,0,20,8.5A3.5,3.5,0,0,0,16.5,5A3.5,3.5,0,0,0,13,8.5A3.5,3.5,0,0,0,16.5,12 M7.5,6.5A2,2,0,0,1,9.5,8.5A2,2,0,0,1,7.5,10.5A2,2,0,0,1,5.5,8.5A2,2,0,0,1,7.5,6.5 M7.5,12A3.5,3.5,0,0,0,11,8.5A3.5,3.5,0,0,0,7.5,5A3.5,3.5,0,0,0,4,8.5A3.5,3.5,0,0,0,7.5,12 M21.5,17.5L14,17.5 14,16.25C14,15.79 13.8,15.39 13.5,15.03 14.36,14.73 15.44,14.5 16.5,14.5 18.94,14.5 21.5,15.71 21.5,16.25 M12.5,17.5L2.5,17.5 2.5,16.25C2.5,15.71 5.06,14.5 7.5,14.5 9.94,14.5 12.5,15.71 12.5,16.25 M16.5,13C15.3,13 13.43,13.34 12,14 10.57,13.33 8.7,13 7.5,13 5.33,13 1,14.08 1,16.25L1,19 23,19 23,16.25C23,14.08,18.67,13,16.5,13z");
@@ -155,6 +160,28 @@ namespace MyCathedra
 
             _fileManager.Move(fileInfo, inputBox.Answer);
 
+            DataGridUpdate();
+        }
+
+        private void CreateFolder(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TitlePath)) return;
+
+            var inputBox = new InputBox("Имя папки");
+            if (inputBox.ShowDialog() != true) return;
+
+            _fileManager.CreateFolder(TitlePath, inputBox.Answer);
+
+            DataGridUpdate();
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            if (!(DataGrid.CurrentItem is FileInfo fileInfo)) return;
+            var messageBoxResult =
+                MessageBox.Show($@"Удалить ""{fileInfo.Name}""?", "Удаление!", MessageBoxButton.YesNo);
+            if (messageBoxResult != MessageBoxResult.Yes) return;
+            _fileManager.Delete(fileInfo);
             DataGridUpdate();
         }
 
